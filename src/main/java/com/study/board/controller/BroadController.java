94e635fa7635b1174eb8ad507d3a78dd0e2627eb
dalhyun2002 <dalhyun2002@naver.com>
2025-07-21 08:20:22 +0900

    처음

---
+++ src/main/java/com/study/board/controller/BroadController.java
@@ -0,0 +1,70 @@
+package com.study.board.controller;
+
+
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BroadController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write") //localhost:8080/board/write
+    public String boardWriterFrom(){
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board, Model model){
+        boardService.write(board);
+        model.addAttribute("message","글 작성이 완료되었습니다");
+        model.addAttribute("searchUrl","/board/list");
+        return "message";
+    }
+
+    @GetMapping("/board/list")
+    public String BoardList(Model model){
+        model.addAttribute("list",boardService.boardList());
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view")
+    public String boardView(Model model, Integer id){
+        model.addAttribute("board",boardService.boardView(id));
+        return "boardview";
+    }
+
+    @GetMapping("board/delete")
+    public String boardDelete(Integer id){
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("board/modify/{id}")
+    public String boardModify(@PathVariable ("id") Integer id, Model model){
+        model.addAttribute("board",boardService.boardView(id));
+        return "boardmodify";
+    }
+
+    @PostMapping ("board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, Model model){
+        Board boardTemp=boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+
+        boardService.write(boardTemp);
+
+        model.addAttribute("message","수정이 완료되었습니다");
+        model.addAttribute("searchUrl","/board/list");
+
+        return "message";
+    }
+
+}
