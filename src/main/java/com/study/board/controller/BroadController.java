3578e247acad6e712df23a8634bad2be89a56727
김달현 <dalhyun2002@naver.com>
2025-07-22 00:05:30 +0900

    끝

--- src/main/java/com/study/board/controller/BroadController.java
+++ src/main/java/com/study/board/controller/BroadController.java
@@ -3,12 +3,19 @@ package com.study.board.controller;
 
 import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
+import jakarta.servlet.ServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class BroadController {
@@ -22,16 +29,33 @@ public class BroadController {
     }
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(Board board, Model model){
-        boardService.write(board);
+    public String boardWritePro(Board board, Model model, @RequestParam(name="file", required = false) MultipartFile file)throws Exception{
+        boardService.write(board, file);
         model.addAttribute("message","글 작성이 완료되었습니다");
         model.addAttribute("searchUrl","/board/list");
         return "message";
     }
 
     @GetMapping("/board/list")
-    public String BoardList(Model model){
-        model.addAttribute("list",boardService.boardList());
+    public String BoardList(Model model, @PageableDefault(page=0, size=10, sort="id", direction = Sort.Direction.DESC) Pageable pageable,
+                            String searchKeyword){
+        Page<Board> list=null;
+
+        if(searchKeyword==null){
+            list=boardService.boardList(pageable);
+        }else {
+            list=boardService.boardSearchList(searchKeyword,pageable);
+        }
+
+        int nowPage=list.getPageable().getPageNumber()+1;
+        int startPage=Math.max(nowPage-4,1);
+        int endPage=Math.min(nowPage+5, list.getTotalPages());
+
+        model.addAttribute("list",list);
+        model.addAttribute("nowPage",nowPage);
+        model.addAttribute("startPage",startPage);
+        model.addAttribute("endPage",endPage);
+
         return "boardlist";
     }
 
@@ -54,12 +78,12 @@ public class BroadController {
     }
 
     @PostMapping ("board/update/{id}")
-    public String boardUpdate(@PathVariable("id") Integer id, Board board, Model model){
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, Model model, @RequestParam(name="file", required = false) MultipartFile file)throws Exception{
         Board boardTemp=boardService.boardView(id);
         boardTemp.setTitle(board.getTitle());
         boardTemp.setContent(board.getContent());
 
-        boardService.write(boardTemp);
+        boardService.write(boardTemp, file);
 
         model.addAttribute("message","수정이 완료되었습니다");
         model.addAttribute("searchUrl","/board/list");
